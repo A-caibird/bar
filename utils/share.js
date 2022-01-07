433e540b21498c57f825966aab21ed12af29602c
letong <1047816691@qq.com>
2022-01-07 10:17:56 +0800

    酒吧朋友圈分享图片优化

--- utils/share.js
+++ utils/share.js
@@ -2,15 +2,20 @@ function commonShare (provider, scene, callback=null) {
 	uni.showLoading({
 		title: '分享中'
 	})
-	let imageUrl = 'https://patixiu.oss-cn-shanghai.aliyuncs.com/202112/20211221/1080x1080-20211221132005936.png'
-	downloadFile(imageUrl).then(res => {
-		console.log(res);
-		let tempFilePath = res.tempFilePath;
-		shareHandle(provider, scene, tempFilePath, callback)
-	}).catch(e => {
-		console.log(e);
-		shareHandle(provider, scene, '', callback);
-	})
+	if(provider == 'weixin'){
+		let imageUrl = 'https://patixiu.oss-cn-shanghai.aliyuncs.com/202201/20220107/1080x1080-20211221132005936-20220107100441756.png'
+		shareHandle(provider, scene, imageUrl, callback)
+	}else{
+		let imageUrl = 'https://patixiu.oss-cn-shanghai.aliyuncs.com/202201/20220107/1080x1080-2-20220107101454498.png'
+		downloadFile(imageUrl).then(res => {
+			console.log(res);
+			let tempFilePath = res.tempFilePath;
+			shareHandle(provider, scene, tempFilePath, callback)
+		}).catch(e => {
+			console.log(e);
+			shareHandle(provider, scene, '', callback);
+		})
+	}
 	
 }
 function shareHandle(provider, scene, imageUrl = "", callback=null){
@@ -27,7 +32,7 @@ function shareHandle(provider, scene, imageUrl = "", callback=null){
 	    scene,
 	    type: 0,
 	    href: href,
-	    title: '爬梯秀APP',
+		title: '爬梯秀APP',
 		summary: '一起来狂欢尬酒呀',
 		imageUrl: imageUrl,
 		success(res) {
