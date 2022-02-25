3820ae929247eb49ac9057e0e2a4fe8e8f0d4e10
letong <1047816691@qq.com>
2022-02-25 16:39:28 +0800

    礼物动画修改

--- components/giftAnimation/giftAnimation.vue
+++ components/giftAnimation/giftAnimation.vue
@@ -1,69 +1,50 @@
 <template>
-	<view class="animation_box" v-if="isShow" @tap="hide">
-		<view class="img_box">
-			<image class="animation_img" :src="gifUrl"></image>
-		</view>
+	<view class="animation_box">
+		<image v-if="showImg" class="animation_img" mode="widthFix" :src="gifUrl" @tap="close"></image>
 	</view>
 </template>
 
 <script>
 	var timeout = null;
-	export default{
+	export default {
 		data() {
 			return {
-				isShow:false,
-				gifUrl:'',
+				gifUrl: '',
+				showImg: false,
 			}
 		},
-		methods:{
-			hide(){
-				this.isShow = false
-				clearTimeout(timeout)
-			},
+		methods: {
 			show(gifUrl){
-				console.log("启动礼物动画");
-				// this.gifUrl = gifUrl
-				// this.isShow = true
-				// timeout = setTimeout(()=>{
-				// 	this.hide()
-				// },5000)
+				this.gifUrl = gifUrl;
+				this.showImg = true;
+				this.$nextTick(() => {
+					timeout = setTimeout(()=>{
+						console.log('关闭礼物弹窗')
+						this.showImg = false;
+					}, 6000)
+				})
+			},
+			clearTimeout() {
+				if(timeout){
+					clearTimeout(timeout)
+				}
+			},
+			close() {
+				// uni.navigateBack()
+				this.clearTimeout()
+				this.showImg = false;
 			},
 		}
 	}
 </script>
 
 <style lang="scss">
-	.animation_box {
-		height: 100%;
-		width: 100%;
+	.animation_img {
 		position: fixed;
-		top: 0rpx;
+		z-index: 200;
+		top: 50%;
 		left: 0rpx;
-		z-index: 1000;
-		background: rgba(0, 0, 0, 0.2);
-
-		.img_box {
-			position: absolute;
-			top: 26%;
-			left: 50%;
-			transform: translateX(-50%);
-
-			display: flex;
-			flex-direction: column;
-			align-items: center;
-
-			.animation_img {
-				height: 440rpx;
-				width: 440rpx;
-			}
-
-			.close_img {
-				height: 54rpx;
-				width: 54rpx;
-				margin-top: 30rpx;
-			}
-		}
-
+		width: 100%;
+		transform: translateY(-50%);
 	}
-
 </style>
--- pages/gift-animation/gift-animation.vue
+++ pages/gift-animation/gift-animation.vue
@@ -1,35 +1,38 @@
 <template>
 	<view class="animation_box" @tap="hide">
-		<view class="img_box">
-			<image class="animation_img" :src="gifUrl"></image>
-			<text class="send-text">{{sendInfo}}</text>
-		</view>
+		<image class="animation_img" mode="widthFix" :src="gifUrl"></image>
 	</view>
 </template>
 
 <script>
 	var timeout = null;
-	export default{
+	export default {
 		data() {
 			return {
-				gifUrl:'',
-				sendInfo:'',
+				gifUrl: '',
+				sendInfo: '',
 			}
 		},
 		onLoad(opt) {
 			this.gifUrl = opt.gifUrl
-			this.sendInfo = opt.sendInfo
-			// timeout = setTimeout(()=>{
-			// 	this.hide()
-			// },5000)
-		},
-		methods:{
-			clearTimeout(){
-				clearTimeout(timeout)
-			},
-			hide(){
-				// this.clearTimeout()
+			// this.sendInfo = opt.sendInfo
+			timeout = setTimeout(()=>{
+				console.log('关闭礼物页面')
 				uni.navigateBack()
+			}, 6000)
+		},
+		methods: {
+			clearTimeout() {
+				if(timeout){
+					clearTimeout(timeout)
+				}
+			},
+			hide() {
+				// uni.navigateBack()
+				this.clearTimeout()
+				uni.redirectTo({
+					url: '/pages/info/gift'
+				})
 			},
 		}
 	}
@@ -39,38 +42,12 @@
 	page {
 		background: transparent;
 	}
-
-	.animation_box {
-		height: 100%;
-		width: 100%;
+	.animation_img {
 		position: fixed;
-		top: 0rpx;
+		z-index: 200;
+		top: 50%;
 		left: 0rpx;
-		z-index: 1000;
-		background: rgba(0, 0, 0, 0.2);
-	
-		.img_box {
-			position: absolute;
-			top: 26%;
-			left: 50%;
-			transform: translateX(-50%);
-	
-			display: flex;
-			flex-direction: column;
-			align-items: center;
-	
-			.animation_img {
-				height: 440rpx;
-				width: 440rpx;
-			}
-	
-			.send-text {
-				margin-top: 30rpx;
-				color: #FFF;
-				font-size: 30rpx;
-			}
-		}
-	
+		width: 100%;
+		transform: translateY(-50%);
 	}
-	
 </style>
